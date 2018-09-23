390b479ce87a80f98fff1fdec80774f9aac53199
Grace Choi <oiograceoio@gmail.com>
2018-09-23 06:12:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   sentence_array.collect do |word|
     new_sentence = word.reverse
+    new_sentence.join(" ")
   end
-  new_sentence.join(" ")
 end
